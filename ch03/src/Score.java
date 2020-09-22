b41617f241dbdde51672fd48e773b5a89f67bd87
whyhardpickname <13zxchen3@gmail.com>
2020-09-23 07:24:44 +0800

    array

---
+++ ch03/src/Score.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @author MarkChern
+ * @create 2020/9/23 7:19
+ */
+public class Score
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of students : ");
+        int numsOfStudents = in.nextInt();
+
+    }
+}
