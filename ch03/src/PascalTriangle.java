6b29181b0676f784318e2cd2fb6013e7e8101606
whyhardpickname <13zxchen3@gmail.com>
2020-09-29 07:37:52 +0800

    array exercises

--- ch03/src/PascalTriangle.java
+++ ch03/src/PascalTriangle.java
@@ -2,26 +2,31 @@
  * @author MarkChern
  * @create 2020/9/28 6:45
  */
-public class PascalTriangle {
-    public static void main(String[] args){
+public class PascalTriangle
+{
+    public static void main(String[] args)
+    {
         int rows = Integer.parseInt(args[0]);
         int[][] pascalTriangle = new int[rows][];
         pascalTriangle[0] = new int[1];
         pascalTriangle[0][0] = 1;
 
-        for (int i = 1; i < pascalTriangle.length; i++){
+        for (int i = 1; i < pascalTriangle.length; i++)
+        {
             pascalTriangle[i] = new int[i + 1];
-            for (int j = 0; j < pascalTriangle[i].length; j++) {
-                if (j == 0 || j == pascalTriangle[i].length - 1) {
-                    pascalTriangle[i][j] = 1;
-                } else {
-                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
-                }
+            pascalTriangle[i][0] = 1;
+            pascalTriangle[i][pascalTriangle[i].length - 1] = 1;
+            for (int j = 1; j < pascalTriangle[i].length - 1; j++)
+            {
+                pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
             }
         }
 
-        for (int i = 0; i < pascalTriangle.length; i++){
-            for (int j = 0; j < pascalTriangle[i].length; j++){
+
+        for (int i = 0; i < pascalTriangle.length; i++)
+        {
+            for (int j = 0; j < pascalTriangle[i].length; j++)
+            {
                 System.out.print(pascalTriangle[i][j] + " ");
             }
             System.out.print("\n");
---
+++ ch03/src/RandomArray.java
@@ -0,0 +1,19 @@
+/**
+ * @author MarkChern
+ * @create 2020/9/29 7:16
+ */
+public class RandomArray
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = i + 1;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] =
+        }
+    }
+}
