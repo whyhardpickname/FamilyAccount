d14e42d04355fd63016ce98bec11fc71d659f7e6
whyhardpickname <13zxchen3@gmail.com>
2020-10-22 07:14:35 +0800

    array test

---
+++ ch03/src/ArrayTest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * @author MarkChern
+ * date 2020/10/22 6:26
+ */
+public class ArrayTest
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {4, 5, 1, 6, 3, 6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
