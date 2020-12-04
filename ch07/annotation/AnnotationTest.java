f328dc63dda3dcda26a398f6d4eb393fc6d4cb0d
machern <13zxchen3@gmail.com>
2020-12-04 08:12:49 +0800

    注解，生成文档

---
+++ ch07/annotation/AnnotationTest.java
@@ -0,0 +1,29 @@
+package annotation;
+
+/**
+ * @author MarkChern
+ * @version 1.0
+ *
+ */
+public class AnnotationTest
+{
+    /**
+     * Java程序主入口
+     * @param args 命令行参数
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+    /**
+     * 返回两数之和
+     * @param a int 第一个数
+     * @param b int 第二个数
+     * @return int 两数之和
+     */
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+}
