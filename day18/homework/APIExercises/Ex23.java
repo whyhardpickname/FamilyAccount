eb8b33a4fcfae27bc6ebbf576cc000e1244334ad
machern <13zxchen3@gmail.com>
2021-05-08 15:52:40 +0800

    仿写ArrayList类

--- day18/homework/APIExercises/Ex23.java
+++ day18/homework/APIExercises/Ex23.java
@@ -1,7 +1,12 @@
 package homework.APIExercises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author MarkChern
  */
 public class Ex23 {
+    public static void main(String[] args) {
+    }
 }
---
+++ day19/test02/MyArrayList.java
@@ -0,0 +1,197 @@
+package test02;
+
+import java.util.Arrays;
+
+/**
+ * @author MarkChern
+ * 动态数组
+ *  增 add(Object o) add(int index, Object o)
+ *  删 remove(int index), remove(Object o)
+ *  改 set(int index, Object o)
+ *  查 indexOf(Object o) size() isEmpty()
+ *  取 get(int index) toArray()
+ */
+public class MyArrayList {
+    /**
+     * 数组元素的个数
+     */
+    private int size;
+
+    /**
+     * 储存元素的数组
+     */
+    private Object[] elementData;
+
+    /**
+     * 默认的数组初始容量
+     */
+    private static final int defaultCapacity = 10;
+
+    /**
+     * 默认数组容量的空数组实例
+     */
+    private static final Object[] defaultCapacityEmptyElementData = {};
+
+    /**
+     * 默认的空数组实例
+     */
+    private static final Object[] defaultEmptyElementData = {};
+
+    /**
+     * 创建指定初始容量的数组
+     * @param initialCapacity 初始容量
+     * @throws IllegalArgumentException 初始容量为负时抛出的异常
+     */
+    public MyArrayList(int initialCapacity) {
+        //先判断是否大于0，是，用指定容量初始化
+        //不是。判断是否等于0，是，用默认空数组初始化
+        //再不是，说明指定容量数字非法，抛出异常
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        }
+        else if (initialCapacity == 0) {
+            this.elementData = defaultEmptyElementData;
+        }
+        else {
+            throw new IllegalArgumentException("illegal capacity: " +
+                                                initialCapacity);
+        }
+    }
+
+    /**
+     * 创建具有默认初始数组容量的数组
+     */
+    public MyArrayList() {
+        this.elementData = defaultCapacityEmptyElementData;
+    }
+
+    /**
+     * 扩容到指定大小。
+     * @param minCapacity 指定的容量
+     */
+    public void ensureCapacity(int minCapacity) {
+        //如果是指定默认的数组，那么容量就是指定的容量。
+        int minExpand = (elementData != defaultEmptyElementData)
+                ? 0 : defaultCapacity;
+
+        if (minCapacity > minExpand)
+            ensureExplicitCapacity(minCapacity);
+    }
+
+    private void ensureCapacityInternal(int minCapacity) {
+        if (elementData == defaultEmptyElementData)
+            minCapacity = Math.max(minCapacity, defaultCapacity);
+
+        ensureExplicitCapacity(minCapacity);
+    }
+
+    private void ensureExplicitCapacity(int minCapacity) {
+        if (minCapacity > elementData.length)
+            grow(minCapacity);
+    }
+
+    /**
+     * 数组最大容量
+     */
+    private static final int maxArraySize = Integer.MAX_VALUE - 8;
+    private void grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0) {
+            newCapacity = minCapacity;
+        }
+        if (newCapacity - maxArraySize > 0) {
+            newCapacity = hugeCapacity(minCapacity);
+        }
+
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    private int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0)
+            throw new OutOfMemoryError();
+        return (minCapacity > maxArraySize) ?
+                Integer.MAX_VALUE :
+                maxArraySize;
+    }
+
+    /**
+     * 获取元素个数
+     * @return 元素个数
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 返回true当数组为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 返回第一个指定元素出现时的下标，如果不存在，返回-1.
+     * @param o 指定元素
+     * @return 下标或-1
+     */
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++)
+                if (elementData == null)
+                    return i;
+        } else {
+            for (int i = 0; i < size; i++)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+    /**
+     * 返回最后一个指定元素出现时的下标，如果不存在，返回-1.
+     * @param o 指定元素
+     * @return 下标或-1
+     */
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--)
+                if (elementData == null)
+                    return i;
+        } else {
+            for (int i = size - 1; i >= 0; i--)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+    /**
+     *
+     * @return 返回恰好容纳所有元素的数组
+     */
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+    /**
+     * 获取指定下标的元素
+     * @param index 指定下标
+     * @return 元素
+     */
+    public Object get(int index) {
+        rangeCheck(index);
+
+        return elementData[index];
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size)
+            throw new ArrayIndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "index: " + index + ", size: " + size;
+    }
+}
